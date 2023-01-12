94552aff2f38ae741f9619b957be24a18b92b557
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-01-12 19:01:59 +0300

    hw done

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ function countNumberOfAlbums() {
   let cards2 = document.querySelectorAll("#hall-of-shame_container .card");
   let theBestAlbum = document.querySelectorAll("#the-best_container img");
   let tapestryAlbums = document.querySelectorAll("#tapestry_container img");
+  let trackListAlbums = document.querySelectorAll(
+    "#review_container table tbody tr"
+  );
 
   return (totalNumOfAlbums =
     cards1.length +
     cards2.length +
     reviewAlbum.length +
     theBestAlbum.length +
-    tapestryAlbums.length);
+    tapestryAlbums.length +
+    trackListAlbums.length);
 }
 
 function showTotalNumberOfAlbums() {
@@ -20,7 +24,7 @@ function showTotalNumberOfAlbums() {
   ).innerText = `Number of albums: ${countNumberOfAlbums()}`;
 }
 
-// function jumbotronFadeIn() { // ** alternative way of fade in animation
+// function jumbotronFadeIn() { // ** alternative way of fade in animation for the jumbotron
 //   document.getElementsByClassName("jumbotron")[0].style.opacity = "100%";
 // }
 
@@ -32,6 +36,54 @@ function deleteFromTrackList() {
       event.target.closest("tr").outerHTML = "";
     });
   }
+  showTotalNumberOfAlbums();
+}
+
+let trackList = document.querySelector("#review_container table tbody");
+function addNewTrack() {
+  let number = document.getElementById("track-num").value;
+  let title = document.getElementById("track-title").value;
+  let artist = document.getElementById("artist-name").value;
+  let duration = document.getElementById("track-dur").value;
+
+  if (number != "" && title != "" && artist != "" && duration != "") {
+    let newTrack = document.createElement("tr");
+    trackList.appendChild(newTrack);
+
+    let trackNumber = document.createElement("th");
+    trackNumber.innerText = number;
+    trackNumber.setAttribute("scope", "row");
+    newTrack.appendChild(trackNumber);
+
+    let trackTitle = document.createElement("td");
+    trackTitle.innerText = title;
+    newTrack.appendChild(trackTitle);
+
+    let trackArtist = document.createElement("td");
+    trackArtist.innerText = artist;
+    newTrack.appendChild(trackArtist);
+
+    let trackDuration = document.createElement("td");
+    trackDuration.innerText = duration;
+    newTrack.appendChild(trackDuration);
+
+    let TDforDeleteButton = document.createElement("td");
+    TDforDeleteButton.innerHTML =
+      '<span class="badge badge-danger">Delete</span>';
+    newTrack.appendChild(TDforDeleteButton);
+
+    deleteFromTrackList(); // function recalled to add event listener to the new added delete buttons
+
+    alert(`${title} added to the track list`);
+  } else {
+    alert("Please first fill the input fields!");
+    return;
+  }
+
+  document.getElementById("track-num").value = "";
+  document.getElementById("track-title").value = "";
+  document.getElementById("artist-name").value = "";
+  document.getElementById("track-dur").value = "";
 }
 
 window.onload = () => {
